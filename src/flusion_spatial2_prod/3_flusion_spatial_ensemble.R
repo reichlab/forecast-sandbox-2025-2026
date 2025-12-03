d439127840255173480c60e152e87f0d8173ac57
Nicholas Reich <nick@umass.edu>
2025-12-02 18:27:59 -0800

    adding production code for spatial2 flusion

---
+++ src/flusion_spatial2_prod/3_flusion_spatial_ensemble.R
@@ -0,0 +1,50 @@
+library(dplyr)
+library(hubData)
+library(hubEnsembles)
+
+
+args <- commandArgs(trailingOnly = TRUE)
+ref_date <- as.Date(args[1])
+
+hub_con <- hubData::connect_model_output("intermediate-output/model-output")
+
+state_models_to_blend <- c("gbqr_3src_spatial2", "AR6_pooled")
+us_models_to_blend <- c("gbqr_3src", "AR6_pooled")
+
+state_dat <- hub_con |>
+  filter(
+    reference_date == ref_date,
+    model_id %in% state_models,
+    location != "US",
+    horizon >= 0
+  ) |>
+  collect_hub()
+
+us_dat <- hub_con |>
+  filter(
+    reference_date == ref_date,
+    model_id %in% us_models,
+    location == "US",
+    horizon >= 0
+  ) |>
+  collect_hub()
+
+ens_model <- bind_rows(state_dat, us_dat) |>
+  simple_ensemble(model_id = "UMass-flusion")
+
+
+# save
+output_dir <- "../../model-output/UMass-flusion"
+
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir, recursive = TRUE)
+}
+
+utils::write.csv(
+  model_out_tbl |> dplyr::select(-model_id),
+  file = file.path(
+    output_dir,
+    paste0(ref_date, "-UMass-flusion.csv")
+  ),
+  row.names = FALSE
+)
